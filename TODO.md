134c1b0daa0de45424ffa0f7f8eb3eba86667aa7
nickklosterman <nick_klosterman@yahoo.com>
2015-02-11 23:58:05 -0500

    static code of addition of objects but that all appears to be working. static array of products though

--- TODO.md
+++ TODO.md
@@ -3,3 +3,7 @@
 # Clean up dead code
 # Select which site to run the auto-lover on; How to select? dropdown? radio button? multipe checkboxes?
 # checkbox to enable clear out the lovelist before adding
+# create productID scraper ; can't use wget to get pages but use `grep "a href=" file.html | sed 's/^.*cover-ups\///;s/&amp.*//;s/\?ProductID=/ /' | uniq | sed 's/^/{"name":"/;s/ /","productID":"/;s/$/"},/'` will get you the productIDs from a page in a format ready for dumping into an array. ***you need to change `cover-ups` to whatever for the page you snagged.
+# add input of id or switch for textbox to allow input of this array data for processing.
+# only enable the plugin on the VS sites.
+# open the https://www.victoriassecret.com/lovelist/view page in a new window to show them what's been added. 
--- popup.js
+++ popup.js
@@ -1,88 +1,107 @@
 function addToLoveList(){
-  var xhr = new XMLHttpRequest();
-  var productArray = [230316, 228684, 2];
-  productArray = [212407, 212606, 226536];
+    var xhr = new XMLHttpRequest();
+    var productArray = [230316, 228684, 2];
+    productArray = [212407, 226536,221475,221349,90647,221819,221444,221440,225919,221370,227153,226284,226286,221777,221781,229071,228154,225585];
+    var interval = 2750;
+    productArray.forEach(function(element,index,array){
+	setTimeout(function(element){
+	    xhr.open('POST','https://www.victoriassecret.com/lovelist/item/heart',true);
+	    var postData="productid="+element+"&ctlg=LJ&cqo=false&path=%2F";
+	    console.log(element+' ' +postData);
+	    // ????? wtf I thought it needed the UID and other info to be passed? or is this being handled automagically since we are on the VS site?	    
+	    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	    xhr.onreadystatechange = function() { //Call a function when the state changes.
+		if(xhr.readyState == 4 && xhr.status == 200) {
+		    console.log(xhr.responseText);
+		}
+	    };
 
-  productArray.forEach(function(element,index,array){
-    xhr.open('POST','https://www.victoriassecret.com/lovelist/item/heart',true);
-    var postData="productid="+element+"&ctlg=LJ&cqo=false&path=%2F";
-    console.log(element+' ' +postData);
-    //    $.post("https://www.victoriassecret.com/lovelist/item/heart", "productid="+element+"&ctlg=LJ&cqo=false&path=%2Fvictorias-secret-sport%2Fsports-bras%2Fseamless-reversible-sport-bra-victorias-secret-sport%3FProductID%3D221633%26CatalogueType%3DOLS")});
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    //xhr.setRequestHeader("Content-length", postData.length); deprecated
-    xhr.onreadystatechange = function() { //Call a function when the state changes.
-      if(xhr.readyState == 4 && xhr.status == 200) {
-        console.log(xhr.responseText);
+	    //do I need a setTimeout call? copy 3 cURL calls to these products, put in a script and see if that works without a pause between calls. Time execution. 
+	    // Is it the missing variables that screw things up?
+	    xhr.send(postData);
+	},interval*index,element); // the back end handling this is atrocious and can't handle things hitting it this quickly. Super Shitty performance. It simply can't handle the requests coming in super fast. I still need to test with curl and see if I get the same results. 
+    });
+    setTimeout(function() { chrome.tabs.reload();},interval*productArray.length); //reload the tab when all  posts are done.
+    //chrome.tabs.create({'url':'https://www.victoriassecret.com/lovelist/view'}); this works but in a way breaks the debugger
+    /*,
+      function( tab) {
+      console.log(tab)
       }
-    }
-
-    //do I need a setTimeout call? copy 3 cURL calls to these products, put in a script and see if that works without a pause between calls. Time execution. 
-    // Is it teh missing variables that screw things up?
-    xhr.send(postData);
-  })
+      };
+    */
 };
 
 var clearSite={
-  "production":"https://www.victoriassecret.com",
-  "test":"https://test-005.lbidts.com",
-  "dev":"https://dev-005.lbidts.com",
-  "pint":"https://vsdpint.lbidts.com"
+    "production":"https://www.victoriassecret.com",
+    "test":"https://test-005.lbidts.com",
+    "dev":"https://dev-005.lbidts.com",
+    "pint":"https://vsdpint.lbidts.com"
 };
 
 //use a heart with a minus or the red circle with diagonal as a symbol to unheart.
 function clearLoveList(cookieObject) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://'+cookieObject.site+'/lovelist/remove/all');
-  xhr.send();
-}
+    cookieObject.site="www.victoriassecret.com";
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://'+cookieObject.site+'/lovelist/remove/all');
+    xhr.send();
+    chrome.tabs.reload();
+};
 
 function getCookie(target) {
-  var mysite="dev-005.lbidts.com";
-  mysite="www.victoriassecret.com";
-  var myObj={"site":mysite,
-             "UID":"",
-             "X-Mapping-omgbnpna":"",
-             "cmTPSet":"",
-             "vShip":"",
-             "VSSESSION":"",
-             "vsPopUnder":""
-            };
-  paramName=[ "UID", "X-Mapping-omgbnpna", "cmTPSet", "vShip", "VSSESSION","vsPopUnder"];
-  chrome.cookies.get({"url":"https://dev-005.lbidts.com","name":"UID"},function(data) {
-    var element =  document.getElementById(target);
-    console.log(data);
-    console.log(data.value);
-    element.innerHTML = data.value;
-  });
+    var mysite="dev-005.lbidts.com";
+    mysite="www.victoriassecret.com";
+    var myObj={"site":mysite,
+	       "UID":"",
+	       "X-Mapping-omgbnpna":"",
+	       "cmTPSet":"",
+	       "vShip":"",
+	       "VSSESSION":"",
+	       "vsPopUnder":""
+	      };
+    paramName=[ "UID", "X-Mapping-omgbnpna", "cmTPSet", "vShip", "VSSESSION","vsPopUnder"];
+    chrome.cookies.get({"url":mysite,"name":"UID"},function(data) {
+	var element =  document.getElementById(target);
+	console.log(data);
+	if (typeof data !== 'undefined' &&  data !== null ) {
+	    console.log(data.value);
+	    element.textContent = data.value;
+	} 
+	
+    });
 
-  chrome.cookies.getAll({},function(cookieList){
-    var clength = cookieList.length;
-    console.log(clength);
-    for (var x=0;x<clength;x++)
-    {
-      if (cookieList[x].domain == mysite) {
-
-        console.log(cookieList[x].name+' / '+cookieList[x].value); //create a key value pair from these two.
-        myObj[cookieList[x].name]=cookieList[x].value;
-      }
-      //        console.log(cookieList[x].name+' / '+cookieList[x].value);
-      //        console.log(cookieList[x].domain);
-    }
-  });
-  console.log(myObj);
-  return myObj;
-}
+    chrome.cookies.getAll({},function(cookieList){
+	var clength = cookieList.length;
+	console.log(clength);
+	for (var x=0;x<clength;x++)
+	{
+	    if (cookieList[x].domain == mysite) {
+		console.log(cookieList[x].name+' / '+cookieList[x].value); //create a key value pair from these two.
+		myObj[cookieList[x].name]=cookieList[x].value;
+	    }
+	    //        console.log(cookieList[x].name+' / '+cookieList[x].value);
+	    //        console.log(cookieList[x].domain);
+	}
+    });
+    console.log(myObj);
+    return myObj;
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("get cookie");
-  var cookieData=getCookie("cookieContent");
-  //clearLoveList(cookieData);
-  addToLoveList();
-  //showPopup();
+    console.log("get cookie");
+    var cookieData=getCookie("cookieContent");
+    //clearLoveList(cookieData);
+    //addToLoveList();
+    //showPopup();
 });
 
-// function showPopup() { //why does this open a billion tabs?
+//two equivalent methods to bind
+document.getElementById('autoLoveList').onclick = addToLoveList; //addEventListener('click',addToLoveList);
+document.getElementById('clearLoveList').addEventListener('click',clearLoveList);
+
+// function showPopup() { //why does this open a billion tabs? I think bc it is executed once per tab that we already have open
 // chrome.tabs.create({'url':'popup.html'});
 // };
 
+
 // document.getElementById("someID").onclick = function () {   };
+
