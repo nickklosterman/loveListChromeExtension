171cb207a2e1bef5cc383e6e3e3bc2296c9de8ec
Nick Klosterman <nick.klosterman@gmail.com>
2015-07-13 17:09:32 -0400

    more image grabbing logic

--- js/productIds.js
+++ js/productIds.js
@@ -1,6 +1,8 @@
-var urlBase = "https://vsdpint.lbidts.com", //"https://www.victoriassecret.com",
+var urlBase = /*"https://vsdpint.lbidts.com", /urlBase =*/ "https://www.victoriassecret.com",
     productObjectArray= new Array(),
-    productImageArray = new Array();
+    productImageArray = new Array(),
+baseImageURI= "//dm.victoriassecret.com/product/760x1013/",
+imageFileExtension=".jpg";
 
 
 function getVS(){
@@ -75,21 +77,48 @@ function parseProduct(res) {
   getImageURLs(); //we really want to run this after productObjectArray is fully populated at the end of parseMenu
 };
 
+/*
+ * 
+ * */
 function getImageURLs() {
   productObjectArray.forEach(function(ele,ind,arr){
     //perform XHR on ele.product-path on the response, grab getElementById("vsImage") and parse out 'data-zoom-src' document.getElementById("vsImage").getAttribute("data-zoom-src")
-    var encodedURL=(urlBase+ele['product-path']).replace(/amp;/,'');
+    var encodedURL=(urlBase+ele['product-path']).replace(/amp;/,''); //I need to escape the ampersand
     XHRGet(encodedURL /*(urlBase+ele['product-path']*/,function(response) {
       var resArray = response.split('\n');
       var productArray = resArray.filter(function(objectEle) {
-	                   if (objectEle.indexOf("data-zoom-src")!==-1){
+	                   if (objectEle.indexOf('data-zoom-src')!==-1 &&
+                               objectEle.indexOf('data-zoom-src=""')==-1 ||
+                              objectEle.indexOf('data-main-image')!==-1 || //data-main-image see note below for data-alt-image
+                              objectEle.indexOf('data-alt-images')!==-1) { //the data-alt-image is the base filename without extension or path e.g. V412464_CROP1  -> https://dm.victoriassecret.com/product/760x1013/V412464_CROP1.jpg
+// $("span[data-alt-image]") is for production
+// $("input[data-main-image]") is for test/dev
 	                     return true;
 	                   } else {
 	                     return false;
 	                   };
                          });
       productArray.forEach(function(ele_,ind_,arr_){
-        var imageURL = ele_.replace(/^.*data-zoom-src="/,'').replace(/".*/,'');
+        var imageURL = ele_.replace(/^.*data-zoom-src="/,'').replace(/^.*data-main-image="/,'' /*urlBase*/).replace(/^.*span  data-alt-image="/,''/*urlBase*/).replace(/".*/,'');
+
+
+/*
+//add on file extension if not present.
+  if (imageURL.indexOf(imageFileExtension) === -1) {
+imageURL+=imageFileExtension;
+}
+//add on base URI if not present.
+if (imageURL.indexOf(baseImageURI) === -1) {
+var temp =baseImageURI+imageURL;
+imageURL= temp
+}
+*/
+
+//assume that if an alt image that it needs the file extension and the baseImageURI
+if (imageURL.indexOf(imageFileExtension) === -1) {
+var temp =baseImageURI+imageURL+imageFileExtension;
+imageURL=temp
+}
         console.log(imageURL);
         productImageArray.push(imageURL);
       });
@@ -105,7 +134,8 @@ function parseMenu(res){
   var resArray = res.split('\n');
   //    console.log(resArray.length);
   //    console.log( typeof resArray[0]);
-  var menuArray = resArray.filter(function(ele) {
+//create array of menu headings/product collections
+/**/  var menuArray = resArray.filter(function(ele) {
 	            
 	            if (ele.indexOf("TOP NAVIGATION ALL AT ONCE")!==-1){
 	              return true;
@@ -113,9 +143,14 @@ function parseMenu(res){
 	              return false;
 	            };
                   });
+/**/
+var menuArray = ['https://www.victoriassecret.com/bras/dream-angels']; //test array ['https://www.victoriassecret.com/bras/dream-angels',['https://www.victoriassecret.com/bras/very-sexy']; 
+// menuArray=['/bras/dream-angels','/bras/very-sexy','/panties/thongs-and-v-strings','/panties/bikinis','/panties/cheekies-and-cheekinis'];
   //    console.log(menuArray.length);
   menuArray.forEach(function(ele,ind,arr) {
     //	console.log(ele.replace(/^.*href="/,'').replace(/".*/,''));
+
+    //create a fully qualified url
     if (ele.indexOf('http://') !== -1 ) {
       arr[ind]=ele.replace(/^.*href="/,'').replace(/".*/,''); //some have a full http in the url
     } else {
@@ -123,9 +158,10 @@ function parseMenu(res){
     }
   });
   menuArray.forEach(function(ele,ind,arr){
-    //console.log(ele);
+    console.log(ele);
     var interval = 100;
     setTimeout(XHRGet,interval*ind,ele,parseProduct); //hammering the site wo an interval makes the site pissy and it shits the bed
+
     //	XHRGet(ele,parseProduct); //actually this might be ok as long as we don't have a url that breaks the XHR get request
   })
   
