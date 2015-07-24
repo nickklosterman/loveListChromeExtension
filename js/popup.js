dd1d9c0361df9bdd7f71a8dc078f71f6eee19aba
Klosterman <nklosterman@c02gycv5dv7l.limited.brands.com>
2015-07-24 14:54:53 -0400

    site status names are clickable links

--- js/popup.js
+++ js/popup.js
@@ -317,6 +317,17 @@ document.getElementById('status').onclick = checkSiteStatus;
 document.addEventListener("DOMContentLoaded", function(event) {
 //check status on startup
     checkSiteStatus();
+    //make links clickable http://stackoverflow.com/questions/8915845/chrome-extension-open-a-link-from-popup-html-in-a-new-tab
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+	(function () {
+	    var ln = links[i];
+	    var location = ln.href;
+	    ln.onclick = function () {
+		chrome.tabs.create({active: true, url: location});
+	    };
+	})();
+    }
 });
 
 
